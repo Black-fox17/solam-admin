f931a0957011d46a3566c7e1f3a9b81806c649c3
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2025-03-17 15:30:29 +0100

    Add console log for credentials in Login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -17,6 +17,7 @@ export default function Login() {
     setLoading(true);
     
     try {
+      console.log(credentials)
       const { data } = await api.post('/auth/login', credentials);
       if (data.status === "success"){
         localStorage.setItem('token', data.data.access_token);
